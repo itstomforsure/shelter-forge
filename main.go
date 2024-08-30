afd933f3a6354a0157fc95a1f220a1fc44514ae7
itstomforsure <wzxzje00@gmail.com>
2024-08-30 19:43:14 +0200

    all

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,26 +21,79 @@ func main() {
 	default:
 		return
 	}
-
-	fmt.Println("args", os.Args)
 }
 
 func handleGenerateSubCommand(cmds []string) {
 	subcmd := cmds[0]
 	switch subcmd {
+	case "a", "all":
+		handleAll(cmds[1])
 	case "i", "interface":
 		fmt.Println("interface")
 		handleInterface(cmds[1:])
-	case "repo":
+	case "r", "repo", "repository":
 		fmt.Println("repository")
+		handleRepository(cmds[1])
+	case "c", "controller":
+		fmt.Println("controller")
 	}
+}
+
+func handleAll(fileName string) {
+	handleRepository(fileName)
+	handleController(fileName)
 }
 
 func handleInterface(cmds []string) {
 	interfaceType := cmds[0]
 	switch interfaceType {
+	case "r", "repo", "repository":
+		fmt.Println("repository")
+		generateRepositoryInterface(cmds[1])
 	case "c", "controller":
-
+		fmt.Println("controller")
 	}
-	fmt.Println(cmds)
+}
+
+func handleRepository(fileName string) {
+	generateRepositoryInterface(fileName)
+	generateRepository(fileName)
+}
+
+func handleController(fileName string) {
+	generateControllerInterface(fileName)
+	generateController(fileName)
+}
+
+func generateRepositoryInterface(fileName string) {
+	generateFile("app/interfaces/repositories/", fileName+".repository.go", "package interfaces\n"+"\ntype I"+strings.Title(fileName)+"Repository interface {}\n")
+}
+func generateRepository(fileName string) {
+	generateFile("repositories/", fileName+".repository.go", "package repositories\n")
+}
+
+func generateControllerInterface(fileName string) {
+	generateFile("app/interfaces/controllers/", fileName+".controller.go", "package controllers\n"+"\ntype I"+strings.Title(fileName)+"Controller interface {}\n")
+}
+func generateController(fileName string) {
+	generateFile("controllers/", fileName+".controller.go", "package contollers\n")
+}
+
+func generateFile(dir string, file string, body string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.MkdirAll(dir, 0700)
+	}
+	f, err := os.Create(dir + file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	l, err := f.WriteString(body)
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
 }
